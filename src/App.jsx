a89ee8cbbe0b74a81658857e779ae24c024ee2f5
SKN178 <s.k.nayeem@gmail.com>
2023-10-25 13:15:37 +0600

    install tailwind css with vite and install daisyui

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-7xl bg-amber-300'>Vite + React</h1>
+
+   
+     
+    </>
+  )
+}
+
+export default App
